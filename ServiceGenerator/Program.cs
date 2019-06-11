6f5435af87ab0849e11aafe3366a80fd1e1a8e67
MUHAMMAD FARHAN FADLY CHEW <farhanchew@gmail.com>
2019-06-11 21:22:05 +0800

    Add project files.

---
+++ ServiceGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ServiceGenerator
+{
+    class Program
+    {
+        public static string Path = "";
+        public static string OriginNamespace = "";
+        public static string TargetNamespace = "";
+        public static DirectoryInfo DirectoryInfo;
+        public static FileInfo[] Files;
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter Target Namespace Name");
+                var TargetNamespace = Console.ReadLine();
+                Console.WriteLine("Enter Domain/Model Path");
+                Path = Console.ReadLine();
+                GetFileInfos();
+                GenerateService();
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        static void GenerateService()
+        {
+            Console.WriteLine($"Generating Services . . .");
+            foreach (FileInfo file in Files)
+            {
+                string contents = File.ReadAllText(file.FullName);
+                string[] lines = contents.Replace("\r", "").Split('\n');
+                Console.Write(contents);
+            }
+        }
+
+        static void GetFileInfos()
+        {
+            DirectoryInfo = new DirectoryInfo(@Path);//Assuming Test is your Folder
+            Files = DirectoryInfo.GetFiles("*.cs"); //Getting Text files
+            if (!Files.Any())
+            {
+                Console.WriteLine("No File Found");
+                return;
+            }
+            string fname = "";
+            foreach (FileInfo file in Files)
+            {
+                fname = fname + ", " + file.Name;
+            }
+            Console.WriteLine($"Found : {fname}");
+        }
+    }
+}
